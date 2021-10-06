66217f15fe6bff04933dad2fbd844ea91c2ee2f0
Emmons Lab <emmons@elegance-server>
2021-10-06 16:59:58 -0400

    fixed the PVS weird_names thing, used dictionary to translate old name to new

--- index.py
+++ index.py
@@ -16,13 +16,12 @@ import matplotlib.pyplot as plt
 def clean_post(post):
     post = post.replace(" ","")
     post_list = post.split(",")
-    post_list = map(clean_name, post_list)
+    post_list = map(clean_neuron_name, post_list)
     post_list = filter(lambda x : x != "" , post_list)
     post_list = sorted(post_list)
     post = ",".join(post_list)
     return post
 
-#also removes the square brackets, treat them as sure
 def clean_neuron_name(neuron):
     ##remove spaces
     neuron = neuron.replace(" ","")
@@ -62,14 +61,16 @@ def convert_name(name):
 weird_names = []
 def good_cellname(name):
     global weird_names
+    global neurons
     if good_cellname_old(name) != (name in neurons):
-        print(name, "; in SI4? : ", name in neurons)
+        print(name + "; in SI4? : " + str(name in neurons))
         weird_names.append(name)
     return name in neurons
 
 ##detecting bad names by manually eliminating
+##only for diagnosing problem
 def good_cellname_old(name):
-    return name[0:3] != "unk" and name[0:3] != "obj" and name[0:6] != "contin" and name[0:4] != "frag"
+    return name == "" and name[0:3] != "unk" and name[0:3] != "obj" and name[0:6] != "contin" and name[0:4] != "frag"
 
 
 ######################################################################
@@ -79,42 +80,44 @@ neurons = pd.read_csv("male_neurons.csv", header=None, comment='#')
 #remove unexpected spaces...
 neurons = neurons[0].apply(lambda x : x.replace(" ",""))
 neurons = neurons.tolist()
-
 ##note to self: DataFrame object doesn't have tolist(),
 ##but a column object does..
-##doing DataFrame.values.tolist() gets a list of lists
-##(one list for each row)
+
 
 #TODO get the contact data from the mysql
 #TODO merge left and right neurons
 
+
 #get synapse list
-df = pd.read_csv('SI-3-Synapse-lists-male.csv')
-df = df.query('EM_series=="N2Y" & type=="chemical"')
-df = df[["pre","post","sections"]]
+syn = pd.read_csv('SI-3-Synapse-lists-male.csv')
+syn = syn.query('EM_series=="N2Y" & type=="chemical"')
+syn = syn[["pre","post","sections"]]
 
 #clean the pre neurons
-df["pre"] = df["pre"].apply(clean_neuron_name)
+syn["pre"] = syn["pre"].apply(clean_neuron_name)
 #clean the post neurons
-df["post"] = df["post"].apply(clean_post)
+syn["post"] = syn["post"].apply(clean_post)
 
 #after cleaning, empty string means unknown
-df_pruned = df[(df.pre != "") & (df.post != "")]
+syn_cleaned = syn[(syn.pre != "") & (syn.post != "")]
 
-##like mysql
-##groupby returns DataFrame, but has weird indexing
-df_grouped = df_pruned.groupby(["pre","post"]).sum().reset_index()
+#sum total number of sections by pre,post
+#(like mysql query)
+#groupby returns DataFrame, has weird indexing (fix with reset_index)
+syn_grouped = syn_cleaned.groupby(["pre","post"]).sum().reset_index()
 
 ##get synapses that are large ( >2 sections)
-df_robust = df_grouped[df_grouped.sections > 2]
+syn_robust = syn_cleaned[syn_cleaned.sections > 2]
+syn_robust_grouped = syn_robust.groupby(["pre","post"]).sum().reset_index()
+#syn_robust = syn_grouped[syn_grouped.sections > 2]
 
 ##count number of post-synaptic types for each neuron
-df_count = df_grouped.groupby(["pre"]).size().reset_index(name="count")
+syn_count = syn_grouped.groupby(["pre"]).size().reset_index(name="count")
 ##graph it
-counts = df_count["counts"]
-numbins = counts.max() - counts.min() + 1
+count_types = syn_count["count"]
+numbins = count_types.max() - count_types.min() + 1
 fig, ax = plt.subplots(1,1)
-ax.hist(counts,bins=numbins)
+ax.hist(count_types,bins=numbins)
 ax.set_title("Distribution of synapse outdegrees (N2Y)")
 ax.set_xlabel("outdegree")
 ax.set_ylabel("frequency")
@@ -122,18 +125,18 @@ plt.show()
 
 ##count number of post-synaptic types for each neuron
 ##but for robust
-df_count_robust = df_robust.groupby(["pre"]).size().reset_index(name="count")
+syn_count_robust = syn_robust_grouped.groupby(["pre"]).size().reset_index(name="count")
 ##graph it
-counts_robust = df_count_robust["count"]
-numbins = counts_robust.max() - counts_robust.min() + 1
+count_types_robust = syn_count_robust["count"]
+numbins = count_types_robust.max() - count_types_robust.min() + 1
 fig, ax = plt.subplots(1,1)
-ax.hist(counts_robust,bins=numbins)
+ax.hist(count_types_robust,bins=numbins)
 ax.set_title("Distribution of synapse outdegrees (N2Y) (robust)")
 ax.set_xlabel("outdegree")
 ax.set_ylabel("frequency")
 plt.show()
 
-##turn "edge list" into adjacency table
+##turn "edge list" into adjacency table before saving
 #(see https://medium.com/@yangdustin5/quick-guide-to-pandas-pivot-table-crosstab-40798b33e367)
 def save_df(df,filename):
     adj_table = pd.crosstab(index=df["pre"],columns=df["post"],values=df["sections"],aggfunc="sum")
@@ -148,34 +151,22 @@ def save_df(df,filename):
 ## experimental
 ################################################################
 
-def ff(row):
-    print("Contin number: ", row.contin_Number)
-    ans = ""
-    for i in range(1,row.partner_Number):
-        print(row["post" + str(i)])
-        ans += row["post" + str(i)]
-    return ans
-
-df.apply(ff, axis=1).head()
-
-
-#get all the post synaptic stuff
-#trying to sort out which values to keep/discard..
-#there are the entries like [CP01],
-#   which are unsure connections so we discard
-# but there's also stuff like unk5590...
-# even unk4733[hyp], contin5934[AVF][AVH][AVJ][PVN]...
-all_post = df["post"].tolist()
-all_post = [x.split(",") for x in all_post]
-all_post = sum(all_post,[])
-all_post = sorted(list(set(all_post)))
-
-all_pre = sorted(list(set(df["pre"].tolist())))
+##get all the post synaptic stuff
+##trying to sort out which values to keep/discard..
+##there are the entries like [CP01],
+##   which are unsure connections so we discard
+## but there's also stuff like unk5590...
+## even unk4733[hyp], contin5934[AVF][AVH][AVJ][PVN]...
+#all_post = df["post"].tolist()
+#all_post = [x.split(",") for x in all_post]
+#all_post = sum(all_post,[])
+#all_post = sorted(list(set(all_post)))
+#all_pre = sorted(list(set(df["pre"].tolist())))
 
 
 ## way to get new dataframe;
 ## axis=1 means apply to rows
 ## 'expand' makes it 3 columns instead of 1
-df.apply(lambda row : [row.pre, sort_post(row.post), row.sections], axis=1, result_type='expand').head()
+#df.apply(lambda row : [row.pre, sort_post(row.post), row.sections], axis=1, result_type='expand').head()
 
 
